d859910bde10752211f563897b6b55ddb5a11aa2
Olga <olga.slepova87@gmail.com>
2025-11-08 19:34:43 +0100

    feat: implement countCats (count '^^' in 2D array)

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,15 @@ const { NotImplementedError } = require('../lib');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  let count = 0;
+
+  for (const row of matrix){
+    for (const cell of row){
+      if (cell === '^^') count += 1;
+    }
+  }
+  return count;
 }
 
 module.exports = {
