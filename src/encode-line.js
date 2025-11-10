5875732939ea1f04c724b0f09669b7db80cb35ef
Olga <olga.slepova87@gmail.com>
2025-11-10 22:42:40 +0100

    feat: implement encode-line.js

--- src/encode-line.js
+++ src/encode-line.js
@@ -11,9 +11,29 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function encodeLine(/* str */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+  if (!str) return ''; //на пустую строку сразу возвращаем ''
+
+  let result = '';
+  let count = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    const current = str[i];
+    const next = str[i + 1];
+
+    if (current === next) {
+      count++;
+    } else {
+      if (count > 1) {
+        result += count + current;
+      } else {
+        result += current;
+      }
+      count = 1;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
