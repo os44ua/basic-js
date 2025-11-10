fea61178f50df2b49cf9e48bc4989bd28cb2b933
Olga <olga.slepova87@gmail.com>
2025-11-11 00:09:34 +0100

    feat: implement vignere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,66 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor(isDirect = true) {
+    this.direct = isDirect !== false; // true прямая, fals обратная
   }
 
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const msg = String(message).toUpperCase();
+    const k = String(key).toUpperCase();
+
+    const A = 'A'.charCodeAt(0);
+    const res = [];
+    let ki = 0;
+
+    for (const ch of msg) {
+      const code = ch.charCodeAt(0);
+      if (code >= 65 && code <= 90) { // [A..Z]
+        const m = code - A;
+        const kv = k.charCodeAt(ki % k.length) - A;
+        const c = (m + kv) % 26;
+        res.push(String.fromCharCode(A + c));
+        ki++;
+      } else {
+        res.push(ch);// остальные другие симводы  кроме букв
+      }
+    }
+
+    const out = res.join('');
+    return this.direct ? out : out.split('').reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const msg = String(message).toUpperCase();
+    const k = String(key).toUpperCase();
+
+    const A = 'A'.charCodeAt(0);
+    const res = [];
+    let ki = 0;
+
+    for (const ch of msg) {
+      const code = ch.charCodeAt(0);
+      if (code >= 65 && code <= 90) { // [A..Z]
+        const c = code - A;
+        const kv = k.charCodeAt(ki % k.length) - A;
+        const m = (c - kv + 26) % 26;
+        res.push(String.fromCharCode(A + m));
+        ki++;
+      } else {
+        res.push(ch);
+      }
+    }
+
+    const out = res.join('');
+    return this.direct ? out : out.split('').reverse().join('');
   }
 }
 
